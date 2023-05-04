bf48f8b3ab0dde514fb96d85107ddd004b2b39d7
Pranish Dahal <pranish.dahal@hvnnet.com>
2023-05-03 20:03:49 -0500

    Setting up statements and results

--- simpledb.go
+++ simpledb.go
@@ -8,8 +8,58 @@ import (
     "runtime"
 )
 
+const(
+    DOT byte = 46
+)
+
 type InputBuffer struct {
     buffer string
+}
+
+type MetaCommandResult string
+const (
+    META_COMMAND_UNRECOGNIZED_COMMAND MetaCommandResult = "Unrecognized_Meta"
+    META_COMMAND_SUCCESS MetaCommandResult = "META_COMMAND_SUCCESS"
+)
+
+type PrepareResult string
+const (
+    PREPARE_UNRECOGNIZED_STATEMENT PrepareResult = "Unrecognized_Prepare"
+    PREPARE_SUCCESS PrepareResult = "PREPARE_SUCCESS"
+)
+
+type StatementType string
+const (
+    STATEMENT_UNDEFINED StatementType ="UNDEFINED"
+    STATEMENT_INSERT StatementType ="INSERT"
+    STATEMENT_SELECT StatementType ="SELECT"
+)
+
+type Statement struct {
+    statementType StatementType
+}
+
+func prepare_statement(input *InputBuffer, statement *Statement) PrepareResult {
+
+    if strings.Compare(input.buffer,"select")==0 {
+        statement.statementType = STATEMENT_SELECT
+        return PREPARE_SUCCESS
+
+    } else if strings.Compare(input.buffer,"insert")==0 {
+        statement.statementType = STATEMENT_INSERT
+        return PREPARE_SUCCESS
+    }
+    return PREPARE_UNRECOGNIZED_STATEMENT
+
+}
+
+func execute_statement(statement *Statement){
+    switch(statement.statementType){
+    case STATEMENT_INSERT:
+        print("INSERTING\n")
+    case STATEMENT_SELECT:
+        print("SELECTING\n")
+    }
 }
 
 func printPrompt(){
@@ -20,9 +70,13 @@ func print(val string){
     fmt.Print(val)
 }
 
-func printI(val int){
-    fmt.Print(val)
+func do_meta_command(input *InputBuffer) MetaCommandResult {
+    if strings.Compare(input.buffer,".exit")==0 {
+        exit(0)
+     }
+     return META_COMMAND_UNRECOGNIZED_COMMAND
 }
+
 
 func readInput(input *InputBuffer) {
     reader := bufio.NewReader(os.Stdin)
@@ -42,16 +96,38 @@ func exit(val int){
 func main() {
     var inputBuffer *InputBuffer
     inputBuffer = &InputBuffer{buffer:"",}
-    c := 0
-    for true {
-        
+    for true {        
         printPrompt()
         readInput(inputBuffer)
-        if strings.Compare(inputBuffer.buffer,".exit")==0 {
-           exit(0)
-        }else{
-            print("Unrecognized command "+inputBuffer.buffer+"\n")
-        }
-        c = c + 1
+
+         if(inputBuffer.buffer[0]==DOT){ 
+            
+            switch do_meta_command(inputBuffer) {
+            case META_COMMAND_SUCCESS:
+                continue
+            case META_COMMAND_UNRECOGNIZED_COMMAND:   
+                print("Unrecognized command "+inputBuffer.buffer+"\n")             
+            default:
+                print("Unrecognized command "+inputBuffer.buffer+"\n")                
+            }
+
+            continue
+
+         }
+
+         var statement *Statement
+         statement = &Statement{statementType:STATEMENT_UNDEFINED,}
+         switch(prepare_statement(inputBuffer,statement)){
+         case PREPARE_SUCCESS:
+            break
+         case PREPARE_UNRECOGNIZED_STATEMENT:
+            fmt.Println(fmt.Sprintf("Unrecognized keyword at start of %s",inputBuffer.buffer))
+        default:
+            fmt.Println(fmt.Sprintf("Unrecognized keyword at start of %s",inputBuffer.buffer))
+         }
+
+
+         execute_statement(statement)
+        
     }
 }
